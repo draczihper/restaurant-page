2fa2c085ff30e0454c188c75ca886fb929f38141
Drac Zihper <draczihper@gmail.com>
2024-06-25 11:37:04 +0300

    Edit the webpack.config for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+	mode: 'development',
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 		assetModuleFilename: '[name][ext]',
+		clean: true,
 	},
+	devtool: 'inline-source-map',
 	module: {
      rules: [
       {
