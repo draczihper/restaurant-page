4f5ac9ac212244810f6d0f660a75ffabd96e30fc
Drac Zihper <draczihper@gmail.com>
2024-06-25 11:38:15 +0300

    Add clearContent func and DOM load

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import { homeContent } from './home.js';
+import { menuContent } from './menu.js';
+import { aboutContent } from './about.js';
 
-homeContent();
+const buttons = document.querySelectorAll('button');
+const contentDiv = document.getElementById('content');
 
 
+// A function to clear the existing content
+function clearContent() {
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+}
 
+// Load the initial content
+document.addEventListener('DOMContentLoaded', () => {
+    homeContent();
+});
 
-console.log("Everything works");
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		clearContent();
+
+		if (button.textContent === "Home") {
+			homeContent();
+		} 
+
+		if (button.textContent === "Menu") {
+			menuContent();
+		}
+
+		if (button.textContent === "About") {
+			aboutContent();
+		}
+	});
+});
