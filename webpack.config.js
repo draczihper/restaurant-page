e31322ffb72a8e21da68ac06fa4b5d3c80973ee8
Drac Zihper <draczihper@gmail.com>
2024-06-27 18:56:48 +0300

    Add css-loader pkg and edit webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
 	devtool: 'inline-source-map',
 	module: {
      rules: [
+     {
+     	test: /\.css$/i,
+     	use: ['style-loader', 'css-loader'],
+     },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
