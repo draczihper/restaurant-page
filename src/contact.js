08b1d71397f69cdcf80fed8eef6ecca6b29f1257
Drac Zihper <draczihper@gmail.com>
2024-07-09 19:45:08 +0300

    Modify map size

--- src/contact.js
+++ src/contact.js
@@ -66,7 +66,7 @@ const contactContent = () => {
 
 	const mapDiv = document.createElement('div');
 	mapDiv.innerHTML = `
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.180985099609!2d39.28154817420411!3d-6.747768393248624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4d665a214a27%3A0x85e7c3ee11c6b03e!2sShoppers%20Plaza%20Masaki!5e0!3m2!1sen!2stz!4v1719317059468!5m2!1sen!2stz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.180985099609!2d39.28154817420411!3d-6.747768393248624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4d665a214a27%3A0x85e7c3ee11c6b03e!2sShoppers%20Plaza%20Masaki!5e0!3m2!1sen!2stz!4v1719317059468!5m2!1sen!2stz" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 `;
 	addressDiv.appendChild(mapDiv);
 
