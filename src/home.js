0a6442d8b5862feb5989728a2463ecfa91fc4229
Drac Zihper <draczihper@gmail.com>
2024-07-21 10:54:17 +0300

    Modify home section

--- src/home.js
+++ src/home.js
@@ -1,29 +1,25 @@
-import caffeImg from './assets/caffe.jpg';
+import caffeImg from "./assets/caffe.jpg";
 
 const homeContent = () => {
 	const contentDiv = document.querySelector("#content");
 
 	const homeDiv = document.createElement("div");
-	homeDiv.setAttribute('class', 'home')
+	homeDiv.setAttribute("class", "home");
 	const homeH1 = document.createElement("h1");
 	homeH1.textContent = "Our Drinks";
 
 	const homePara = document.createElement("p");
-	homePara.textContent = "When it comes to the important stuff - the drink - we are committed to sourcing and serving is one of the highest quality and we believe is nothing short of the best drink possible";
-
+	homePara.textContent =
+		"When it comes to the important stuff - the drink - we are committed to sourcing and serving is one of the highest quality and we believe is nothing short of the best drink possible";
 
 	const imgDiv = document.createElement("div");
 	imgDiv.setAttribute("class", "background");
 	homeDiv.appendChild(imgDiv);
 
-
-
 	homeDiv.appendChild(homeH1);
 	homeDiv.appendChild(homePara);
 
 	return contentDiv.appendChild(homeDiv);
-}
-
-
+};
 
 export { homeContent };
